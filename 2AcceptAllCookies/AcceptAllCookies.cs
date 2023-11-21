a9fd85fd4f14b3596250f41bda6f65d4e30bc4fd
Fharizanov <36274652+Fharizanov@users.noreply.github.com>
2023-11-21 11:25:56 +0200

    Add files via upload

---
+++ 2AcceptAllCookies/AcceptAllCookies.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace FilipTest1
+{
+    class AcceptAllCookies
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://digitall.com/";
+            try
+            {
+                IWebElement element = driver.FindElement(By.XPath("//*[@id=\"cookiescript_accept\"]"));
+                Console.WriteLine("The 'ACCEPT ALL' button of the page is found and is: " + element);
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine("Assertion failed with the following exception:\n" + ex);
+            }
+
+            try
+            {
+                IWebElement element1 = driver.FindElement(By.XPath("//*[@id=\"cookiescript_accept\"]"));
+                element1.Click();
+                Console.WriteLine("The 'ACCEPT ALL' button of the page is found and has been clicked");
+            }
+            catch (NoSuchElementException ex1)
+            {
+                Console.WriteLine("Assertion failed with the following exception1:\n" + ex1);
+            }
+
+            driver.Quit();
+        }
+    }
+}
