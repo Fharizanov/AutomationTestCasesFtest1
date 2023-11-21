6518dff28761b18fe4c2923cbbdccbe28bfaec07
Fharizanov <36274652+Fharizanov@users.noreply.github.com>
2023-11-21 11:23:53 +0200

    Add files via upload

---
+++ 1LoadHomePage/LoadHomePage.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilipTest1
+{
+    class LoadHomePage
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://digitall.com/";
+            try
+            {
+                IWebElement element = driver.FindElement(By.XPath("//*[@id=\"digitall__logo\"]"));
+                Console.WriteLine("The logo of the page is found and is: " + element);
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine($"Assertion failed with the following message:\n" + ex);
+            }
+
+            driver.Quit();
+        }
+    }
+}
